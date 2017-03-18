9f6cd91774077951f015278de478125c64712dae
test <test@unknown10ddb1a3c7c0>
2017-03-18 18:04:12 +0100

    aufgabe_0

---
+++ Aufgabe_0/Aufgabe_0.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var name = prompt("Bitte geben Sie Ihren Namen ein.");
+    var gruss = "Herzlich Willkommen ";
+    document.body.innerHTML = gruss + name;
+});
+//# sourceMappingURL=Aufgabe_0.js.map
---
+++ Aufgabe_0/Aufgabe_0.ts
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var name = prompt("Bitte geben Sie Ihren Namen ein.");
+    var gruss = "Herzlich Willkommen ";
+    document.body.innerHTML = gruss + name;
+});
