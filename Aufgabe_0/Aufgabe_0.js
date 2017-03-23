b072014c5da30d7029b1e96eeb0cd99354328c6f
test <test@tests-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-23 16:34:18 +0100

    .

--- Aufgabe_0/Aufgabe_0.js
+++ Aufgabe_0/Aufgabe_0.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var name = prompt("Bitte geben Sie Ihren Namen ein.");
+    var name  = prompt("Bitte geben Sie Ihren Namen ein.");
     var gruss = "Herzlich Willkommen ";
     document.body.innerHTML = gruss + name;
 });
--- Aufgabe_0/Aufgabe_0.ts
+++ Aufgabe_0/Aufgabe_0.ts
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', function () {
     var name : string = prompt("Bitte geben Sie Ihren Namen ein.");
     var gruss : string = "Herzlich Willkommen ";
     document.body.innerHTML = gruss + name;
-});
+}); 
