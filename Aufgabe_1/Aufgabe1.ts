89e095d6dfac69892f0bfd5699b502fc2561cf3a
test <test@tests-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-23 15:51:55 +0100

    .

--- Aufgabe_1/Aufgabe1.ts
+++ Aufgabe_1/Aufgabe1.ts
@@ -26,8 +26,6 @@ for (let i: number = 0; i < n; i++) {
     }
 }
 
-
-
 function placeDiv(_color: string, _x: number, _y: number, _width: number, _height: number): void {
     let div: HTMLDivElement = document.createElement("div");
     document.body.appendChild(div);
