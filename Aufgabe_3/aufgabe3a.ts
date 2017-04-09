1c11546e0eb190101f60c5f33b55613d17e775f3
test <test@tests-mbp>
2017-04-09 17:29:03 +0200

    3a 3b

---
+++ Aufgabe_3/aufgabe3a.ts
@@ -0,0 +1,132 @@
+/* Aufgabe: Aufgabe 3a
+Name: Mario Sommer
+Matrikel: 254785
+Datum: 09.04.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let zeile: number = 0;
+    let rice: number = 1;
+
+    for (let i: number = 0; i < 64; i++) {
+
+        let div: HTMLElement = document.createElement("div");
+
+        if (i % 8 == 0) {
+            div.className = "floating";
+            zeile++;
+        }
+
+        //ungerade Zeil  
+        if (zeile % 2 == 0) {
+
+            if (i % 2 == 0) {
+                div.className = "board black";
+            }
+            else {
+                div.className = "board white";
+            }
+        }
+
+        //gerade Zeil  
+        if (zeile % 2 == 1) {
+
+            if (i % 2 == 0) {
+                div.className = "board white";
+            }
+            else {
+                div.className = "board black";
+            }
+        }
+
+        div.innerText = "" + rice;
+        rice *= 2;
+        document.body.appendChild(div);
+    }
+
+
+
+
+    //    var divList = document.getElementsByTagName("div");
+    //    var state =e; 
+    
+    //    for (let i_2: number = 0; i_2 < 8; i_2 ++) {
+
+    //        divList[i_2].addEventListener("click"on () {
+        
+    //            .log(state);
+       
+    //        if (state) {
+    //            document.getElementsByTagName("div")[i_2].style.backgroundColor = "blue";
+    //       te;
+    //        }
+        
+        
+    //        else {
+    //            document.getElementsByTagName("div")[i_2].style.backgroundColor = "";
+    //            sta true;
+    //        }
+    //        });
+    //    }  
+
+    
+    document.addEventListener("mousemove", function(event) {
+
+        var x = event.pageX;
+        var y = event.pageY;
+
+        // document.getElementById("position").innerHTML = " X = "+ x +"][ Y = y;
+   
+        // Feld erstellen
+        let position = document.createElement("p");
+        document.body.appendChild(position);
+        position.id = "position";
+
+        document.getElementById("position").style.position = "absolute";
+        document.getElementById("position").style.top = y + "px";
+        document.getElementById("position").style.left = x + "px";
+        document.getElementById("position").style.border = "solid #AFEEEE";
+        document.getElementById("position").style.color = "black";
+        document.getElementById("position").style.background = "#AFEEEE";
+    });
+
+    var felder = document.getElementsByTagName("div");
+    var ausgewaehlteFelder = document.getElementsByClassName("ausgewaehlteFelder");
+
+    // clickEvent für die ersten 8Felder
+    for (var i = 0; i < 8; i++) {
+        felder[i].addEventListener("click", function() {
+            this.classList.toggle("ausgewaehlteFelder");    //ausgewaehlte Felder sollen die Klasse "" bekommen
+            koerner(); //Funktion ausführen
+        });
+    }
+
+    function koerner() {
+
+        var anzahlKoerner = 0;
+
+        //    document.getElementById("position").style.background = "#AFEEEE";
+
+        if (ausgewaehlteFelder.length > 0) {
+            document.getElementById("position").style.display = "inline-block"; // bei mindestens einem ausgewählten Feld Box einblenden
+        }
+
+        if (ausgewaehlteFelder.length <= 0) {
+            document.getElementById("position").style.display = "none"; // bei 0 ausgewählten Feldern Box ausblenden
+        }
+
+
+        //Inhalt der ausgewaehlten Felder im Dezimal- und Hexadezimalsystem ausrechnen
+        for (var i = 0; i < ausgewaehlteFelder.length; i++) {
+            anzahlKoerner += Number(ausgewaehlteFelder[i].innerHTML);
+            document.getElementById("position").innerHTML = "Reiskörner:" + "\r\n" + "Dezimal: " + anzahlKoerner.toString() + "\r\n" + "Hexadezimal: " + anzahlKoerner.toString(16);
+        }
+    }
+});
+
+
+
---
+++ Aufgabe_3/aufgabe3b.js
@@ -0,0 +1,61 @@
+/* Aufgabe: Aufgabe 3b
+Name: Mario Sommer
+Matrikel: 254785
+Datum: 09.04.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    //Arrays erstellen
+    var spielkarten = ["Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As", "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik As"];
+    var handkartenArray = [];
+    var ablegestapelArray = [];
+    //Divs erstellen
+    var divZiehstapel = document.createElement("div");
+    document.body.appendChild(divZiehstapel);
+    divZiehstapel.id = "divZiehstapel";
+    divZiehstapel.innerHTML = "Ziehstapel";
+    var handkarten = document.createElement("div");
+    document.body.appendChild(handkarten);
+    handkarten.id = "handkarten";
+    var divAblagestapel = document.createElement("div");
+    document.body.appendChild(divAblagestapel);
+    divAblagestapel.id = "divAblagestapel";
+    divAblagestapel.innerHTML = "Ablagestapel";
+    var i = spielkarten.length;
+    //Karte ziehen
+    document.getElementById("divZiehstapel").addEventListener("click", function () {
+        if (spielkarten.length > 0 && handkartenArray.length < 5) {
+            i--;
+            var zufallszahl = Math.floor((Math.random() * i) + 0); //Zufallszahl
+            var karteZiehen = spielkarten[zufallszahl];
+            spielkarten.splice(zufallszahl, 1); // Karte aus Spielkartenstapel-Array löschen
+            handkartenArray.push(karteZiehen); // Karte dem Hand-Array hinzufügen
+            console.log(handkartenArray);
+            console.log(spielkarten);
+            console.log(zufallszahl);
+            console.log(ablegestapelArray);
+            var divHandkarte = document.createElement("div"); // Handkarte 1-5 erstellen
+            document.getElementById("handkarten").appendChild(divHandkarte);
+            divHandkarte.style.backgroundColor = "grey";
+            divHandkarte.style.width = "10em";
+            divHandkarte.style.height = "16em";
+            divHandkarte.style.cssFloat = "left";
+            divHandkarte.style.margin = "1em";
+            divHandkarte.textContent = karteZiehen;
+            divHandkarte.addEventListener("click", function () {
+                for (var i_1 = 0; i_1 < handkartenArray.length; i_1++) {
+                    if (this.textContent == handkartenArray[i_1]) {
+                        ablegestapelArray.push(handkartenArray[i_1]); // fügt die Karte dem Ablagestapel Array hinzu
+                        handkartenArray.splice(i_1, 1); // entfernt die geklickte Karte aus dem Handkarten Array
+                        break;
+                    }
+                }
+                //       document.getElementById("divAblagestapel").textContent = handkartenArray[i]; 
+                this.parentNode.removeChild(this); // Entfernt das Div 
+            });
+        }
+    });
+});
+//# sourceMappingURL=aufgabe3b.js.map
